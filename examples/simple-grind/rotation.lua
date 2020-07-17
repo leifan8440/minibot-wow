ddb42ca53bce663854e8173ffe77f1b367aaa131
Pierre Picard <pierre.picard@protonmail.com>
2020-07-17 17:27:15 +0800

    init

---
+++ examples/simple-grind/rotation.lua
@@ -0,0 +1,19 @@
+﻿return {
+    -- Define rotation.
+    rotations = {
+      combat = function(env, is_pulling)
+        -- called when a target unit is selected to engage.
+        if (UnitExists("target")) then
+            RunMacroText("/startattack");
+            if (UnitPower("player") > 15) then
+                RunMacroText("/cast Heroic Strike");
+            end
+        end
+      end,
+      prepare = function(env)
+        -- called to check and run the preparation rotation, such as summon and feeding pets, buffing, conjuring and etc.
+        -- Return true if preparation is still ongoing.
+        return false;
+      end,
+    }
+  };

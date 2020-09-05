d2c3505af033c47d88bdb5d7b3451abedc6358e6
lnrdhmmr <70632855+lnrdhmmr@users.noreply.github.com>
2020-09-05 00:31:10 -0700

    Update LibDraw.lua

--- examples/LibDraw/LibDraw.lua
+++ examples/LibDraw/LibDraw.lua
@@ -1,16 +1,14 @@
 if not MBLibDraw then
 	MBLibDraw = {
 	line = {r = 0, g = 1, b = 0, a = 1, w = 1},
-	callbacks = {},
 	canvas = CreateFrame("Frame", WorldFrame),
 	lines = {},
-	lines_used = {}
+	fontstrings = {},
 	}
 	MBLibDraw.canvas:SetAllPoints(WorldFrame)
-	local WorldToScreen_Original = WorldToScreen
 end
-local function WorldToScreen(wX, wY, wZ)
-	local sX, sY = WorldToScreen_Original(wX, wY, wZ)
+local function MBWorldToScreen(wX, wY, wZ)
+	local _, sX, sY = wmbapi.WorldToScreen(wX, wY, wZ)
 	local a = 1365;
 	local b = 768;
 	if sX and sY then
@@ -26,8 +24,8 @@ end
 function Draw2DLine(sx, sy, ex, ey, width)
 	if not ey then
 		width = ex
-		sx, sy = WorldToScreen(ObjectPosition(sx))
-		ex, ey = WorldToScreen(ObjectPosition(sy))
+		ex, ey = MBWorldToScreen(wmbapi.ObjectPosition(sy))
+		sx, sy = MBWorldToScreen(wmbapi.ObjectPosition(sx))
 	end
 	local L = tremove(MBLibDraw.lines) or nil
 	if not L then
@@ -35,7 +33,6 @@ function Draw2DLine(sx, sy, ex, ey, width)
 		L.line = L:CreateLine()
 		L.line:SetDrawLayer("BACKGROUND")
 	end
-	tinsert(MBLibDraw.lines_used, L)
 	L:ClearAllPoints()
 	if (sx > ex and sy > ey) or (sx < ex and sy < ey) then
 		L:SetPoint("TOPRIGHT", MBLibDraw.canvas, "TOPLEFT", sx, sy)
@@ -63,25 +60,12 @@ function Draw2DLine(sx, sy, ex, ey, width)
 	L:Show()
 end
 function Draw2DText(x,y,text,font)
-	if x and y then
-		local F = tremove(MBLibDraw.fontstrings) or MBLibDraw.canvas:CreateFontString(nil, "BACKGROUND")
-		if font then
-			F:SetFontObject(font)
-		end
-		F:SetText(text)
-		F:SetTextColor(MBLibDraw.line.r, MBLibDraw.line.g, MBLibDraw.line.b, MBLibDraw.line.a)
-		if p then
-			local width = F:GetStringWidth() - 4
-			local offsetX = width*0.5
-			local offsetY = F:GetStringHeight() + 3.5
-			local pwidth = width*p*0.01
-			FHAugment.drawLine(x-offsetX, y-offsetY, (x+offsetX), y-offsetY, 4, r, g, b, 0.25)
-			FHAugment.drawLine(x-offsetX, y-offsetY, (x+offsetX)-(width-pwidth), y-offsetY, 4, r, g, b, 1)
-		end
-		F:SetPoint("TOPLEFT", UIParent, "TOPLEFT", x-(F:GetStringWidth()*0.5), y)
-		F:Show()
-		tinsert(MBLibDraw.fontstrings_used, F)
-	end
+	local F = tremove(MBLibDraw.fontstrings) or MBLibDraw.canvas:CreateFontString(nil, "BACKGROUND")
+	F:SetFont("Fonts\\FRIZQT__.TTF",font or 12)
+	F:SetText(text)
+	F:SetTextColor(MBLibDraw.line.r, MBLibDraw.line.g, MBLibDraw.line.b, MBLibDraw.line.a)
+	F:SetPoint("TOPLEFT", UIParent, "TOPLEFT", x-(F:GetStringWidth()*0.5), y)
+	F:Show()
 end
 function SetDrawColor(r, g, b, a) 
 	MBLibDraw.line.r = r * 0.00390625	
@@ -93,28 +77,4 @@ function SetDrawColor(r, g, b, a)
 		MBLibDraw.line.a = 1
 	end
 	MBLibDraw.line.w = 5
-end
-local function Sync(callback)
-	tinsert(MBLibDraw.callbacks, callback)
-end
-local function clearCanvas()
-	for i = #MBLibDraw.lines_used, 1, -1 do
-		MBLibDraw.lines_used[i]:Hide()
-		tinsert(MBLibDraw.lines, tremove(MBLibDraw.lines_used))
-	end
-	for i = #MBLibDraw.fontstrings_used, 1, -1 do
-		MBLibDraw.fontstrings_used[i]:Hide()
-		tinsert(MBLibDraw.fontstrings, tremove(MBLibDraw.fontstrings_used))
-	end
-end
-local function OnDrawUpdate()
-	if MBLibDraw and IsPlayerInWorld() then
-		clearCanvas()
-		for _, callback in ipairs(MBLibDraw.callbacks) do
-			callback()
-		end
-	end
-end
-local function Enable(interval)
-	return C_Timer.NewTicker(interval, OnDrawUpdate)
 end
